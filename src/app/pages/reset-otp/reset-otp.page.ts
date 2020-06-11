c19983b4d1b7bf07cd976af6c9c990c111eb3a8c
suraj <suraj@mindpathtech.com>
2020-06-11 23:00:05 +0530

    changes in otp

--- src/app/pages/reset-otp/reset-otp.page.ts
+++ src/app/pages/reset-otp/reset-otp.page.ts
@@ -15,8 +15,8 @@ import { AuthService } from 'src/shared/services/auth.service';
 export class ResetOtpPage implements OnInit {
   verifyForm: FormGroup;
   opt: any;
-  isSubmitted = false;
-  otps:any='';
+  isSubmitted = true;
+  otps: any = '';
   config = {
     allowNumbersOnly: true,
     length: 4,
@@ -26,23 +26,23 @@ export class ResetOtpPage implements OnInit {
     inputStyles: {
       'width': '14vw',
       'height': '14vw',
-      'margin':' 0 5px',
-        'text-align': 'center',
-        'font-size': '9vw',
-        'border': 'solid 1px #ccc',
-        'box-shadow':' 0 0 5px #ccc inset',
-        'outline': 'none',
+      'margin': ' 0 5px',
+      'text-align': 'center',
+      'font-size': '9vw',
+      'border': 'solid 1px #ccc',
+      'box-shadow': ' 0 0 5px #ccc inset',
+      'outline': 'none',
     },
   };
-  @ViewChild('otp1',{static:false}) el1:ElementRef;
-  @ViewChild('otp2',{static:false}) el2:ElementRef;
-  @ViewChild('otp3',{static:false}) el3:ElementRef;
-  @ViewChild('otp4',{static:false}) el4:ElementRef;
+  @ViewChild('otp1', { static: false }) el1: ElementRef;
+  @ViewChild('otp2', { static: false }) el2: ElementRef;
+  @ViewChild('otp3', { static: false }) el3: ElementRef;
+  @ViewChild('otp4', { static: false }) el4: ElementRef;
 
   constructor(
-    private rd: Renderer2, 
-    public CS:CommonService ,
-    public auth: AuthService ,private formBuilder: FormBuilder, private router: Router, private commonService: CommonService, private userService: UserService) {
+    private rd: Renderer2,
+    public CS: CommonService,
+    public auth: AuthService, private formBuilder: FormBuilder, private router: Router, private commonService: CommonService, private userService: UserService) {
     this.verifyForm = this.formBuilder.group({
       otp1: ['', Validators.required],
       otp2: ['', Validators.required],
@@ -50,43 +50,48 @@ export class ResetOtpPage implements OnInit {
       otp4: ['', Validators.required],
     });
     CS.presentLoading('')
-    if(this.auth.getLoggedUser()){
+    if (this.auth.getLoggedUser()) {
       CS.dismissLoading();
       router.navigateByUrl('/dashboard/tab1')
-    }else{
+    } else {
       CS.dismissLoading();
     }
   }
 
   ngOnInit() {
-    
+
 
   }
 
-  Type(T){
+  Type(T) {
     console.log()
-   if(T==1&&this.verifyForm.value.otp1.length==1){this.el2.nativeElement.focus()};
+    if (T == 1 && this.verifyForm.value.otp1.length == 1) { this.el2.nativeElement.focus() };
   }
 
-  onOtpChange(event){
-    this.otps=event;
-   }
+  onOtpChange(event) {
+    this.otps = event;
+    if (this.otps.length == 4) {
+      this.isSubmitted = false;
+    }
+    else if (this.otps.length < 4) {
+      this.isSubmitted = true;
+    }
+  }
   Verify() {
-    let str5 =this.otps;
-    this.isSubmitted = true;
-    if (str5.length<4) {
+    let str5 = this.otps;
+    if (str5.length < 4) {
       this.commonService.Toaster('Please enter otp!', 'danger')
       return false;
-    } else if(str5.length==4) {
-    this.commonService.presentLoading('');
+    } else if (str5.length == 4) {
+      this.commonService.presentLoading('');
       this.userService.verifyOtp({ otp: str5, phone_number: localStorage.getItem('phone'), user_type: "u" }, '/authentication/verify-otp').subscribe(res => {
         this.commonService.Toaster('Otp Verification successfull!', 'success')
         this.router.navigateByUrl('forgot-password')
-    this.commonService.dismissLoading();
+        this.commonService.dismissLoading();
 
       }, error => {
         this.commonService.Toaster(error.error.message, 'danger')
-    this.commonService.dismissLoading();
+        this.commonService.dismissLoading();
 
       })
     }
--- src/app/pages/verification/verification.page.ts
+++ src/app/pages/verification/verification.page.ts
@@ -15,8 +15,8 @@ import { AuthService } from 'src/shared/services/auth.service';
 export class VerificationPage implements OnInit {
   verifyForm: FormGroup;
   opt: any;
-  isSubmitted = false;
-  otps:any='';
+  isSubmitted = true;
+  otps: any = '';
   config = {
     allowNumbersOnly: true,
     length: 4,
@@ -26,18 +26,18 @@ export class VerificationPage implements OnInit {
     inputStyles: {
       'width': '14vw',
       'height': '14vw',
-      'margin':' 0 5px',
-        'text-align': 'center',
-        'font-size': '9vw',
-        'border': 'solid 1px #ccc',
-        'box-shadow':' 0 0 5px #ccc inset',
-        'outline': 'none',
+      'margin': ' 0 5px',
+      'text-align': 'center',
+      'font-size': '9vw',
+      'border': 'solid 1px #ccc',
+      'box-shadow': ' 0 0 5px #ccc inset',
+      'outline': 'none',
     },
   };
   constructor(private formBuilder: FormBuilder,
-     private router: Router,
-    public auth: AuthService ,
-      private commonService: CommonService, private userService: UserService) {
+    private router: Router,
+    public auth: AuthService,
+    private commonService: CommonService, private userService: UserService) {
     this.verifyForm = this.formBuilder.group({
       otp1: ['', Validators.required],
       otp2: ['', Validators.required],
@@ -45,38 +45,43 @@ export class VerificationPage implements OnInit {
       otp4: ['', Validators.required],
     });
     commonService.presentLoading('')
-    if(this.auth.getLoggedUser()){
+    if (this.auth.getLoggedUser()) {
       commonService.dismissLoading();
       router.navigateByUrl('/dashboard/tab1')
-    }else{
+    } else {
       commonService.dismissLoading();
     }
   }
-   
+
   ngOnInit() {
   }
-  onOtpChange(event){
-   this.otps=event;
+  onOtpChange(event) {
+    this.otps = event;
+    if (this.otps.length == 4) {
+      this.isSubmitted = false;
+    }
+    else if (this.otps.length < 4) {
+      this.isSubmitted = true;
+    }
   }
 
   Verify() {
-    let str5 =this.otps;
-    this.isSubmitted = true;
-    
-    if (str5.length<4) {
+    let str5 = this.otps;
+
+    if (str5.length < 4) {
       this.commonService.Toaster('Please enter otp!', 'danger')
       return false;
-    } else if(str5.length==4) {
-    this.commonService.presentLoading('');
+    } else if (str5.length == 4) {
+      this.commonService.presentLoading('');
       this.userService.verifyOtp({ otp: str5, phone_number: localStorage.getItem('phone'), user_type: "u" }, '/authentication/verify-user').subscribe(res => {
         this.commonService.Toaster('Otp Verification successfull!', 'success')
         localStorage.removeItem('phone');
         this.router.navigate(['login'])
-      this.commonService.dismissLoading()
+        this.commonService.dismissLoading()
 
       }, error => {
         this.commonService.Toaster(error.error.message, 'danger')
-      this.commonService.dismissLoading()
+        this.commonService.dismissLoading()
 
       })
     }
